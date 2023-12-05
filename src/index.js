6c9bb698f8656ccdcf8de93b2e476334eaf61698
DianaR <drangel@grupoesquimal.mx>
2023-12-05 13:55:02 -0600

    Feat: Routers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express(); 
 
-app.listen(3000);
-console.log('server on port 3000')
+//Middlewares: funciones que se ejecutan antes de llegar a rutas 
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); //urlencode procesa un dato de formulario y lo combierte en objeto 
+
+
+
+//routers
+app.use(require('./routers/index.js'));
+
+app.listen(4000);
+console.log('server on port 4000');
